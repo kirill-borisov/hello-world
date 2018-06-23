50cf8c3c91e9a342987e84b910ad100cc75f792e
kirill-borisov <40303859+kirill-borisov@users.noreply.github.com>
2018-06-24 00:39:06 +0200

    Edited in GitHub!!

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,6 @@ public class helloworld
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
-		System.out.println("Edited in GitHub!");
+		System.out.println("Edited in GitHub!!");
 	}
 }
